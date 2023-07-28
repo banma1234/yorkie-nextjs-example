ab8ccee1c26fa5f427817fd5c200992f10501a86
chocoham <banma1234@gmail.com>
2023-07-29 02:26:27 +0900

    feat: react-calendar 추가'

--- app/editor/textEditor.tsx
+++ app/editor/textEditor.tsx
@@ -1,23 +1,29 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
+
 import { DocTypes } from "../utils/types";
+import Calendar from "react-calendar";
+import 'react-calendar/dist/Calendar.css'
 
 export default function TextEditor(props: DocTypes) {
   const { content, peers } = props;
 
   return (
     <>
-      <h1>hi</h1>
-      <hr />
       <ul>
         {peers.map((item: any) => {
           return <li>{item}</li>;
         })}
       </ul>
-      <br />
+      <hr />
       <div>
         <p>{JSON.stringify(content)}</p>
+        <Calendar 
+          formatDay={(locale, date) =>
+            date.toLocaleString('en', { day: 'numeric' })
+          }
+        />
       </div>
     </>
   );
